a2301923714da8b37a10188b9365a28c907dc237
bl4ckl4b3l <108445788+bl4ckl4b3l@users.noreply.github.com>
2023-05-23 14:52:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Function to add a product to the shopping cart
+function addToCart(product) {
+    // Add the product to the shopping cart
+    // Implement your logic here
+    console.log(`Product "${product}" added to the cart.`);
+  }
+  
+  // Function to remove a product from the shopping cart
+  function removeFromCart(product) {
+    // Remove the product from the shopping cart
+    // Implement your logic here
+    console.log(`Product "${product}" removed from the cart.`);
+  }
+  
+  // Function to process the checkout
+  function checkout() {
+    // Process the checkout
+    // Implement your logic here
+    console.log("Checkout completed.");
+  }
+  
+  // Example usage
+  addToCart("T-Shirt");
+  removeFromCart("Hoodie");
+  checkout();
